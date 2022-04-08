fc627eb80374da90b2ae7b6f17e42ddbfd99195b
Wei <wei.peng.0404@gmail.com>
2022-04-08 19:05:23 +0200

    Add output report in demo4nilar.

--- tools/demo4nilar.py
+++ tools/demo4nilar.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import time
 from loguru import logger
+import json
 
 import cv2
 from lxml import etree
@@ -13,7 +14,7 @@ from yolox.data.data_augment import ValTransform
 from yolox.data.datasets import NILAR_CLASSES
 from yolox.exp import get_exp
 from yolox.tools.demo import Predictor, image_demo
-from yolox.utils import get_model_info
+from yolox.utils import get_model_info, vis
 
 IMAGE_EXT = [".jpg", ".jpeg", ".webp", ".bmp", ".png"]
 
@@ -22,7 +23,7 @@ def make_parser():
     parser.add_argument("demo", default="image", help="demo type")
     parser.add_argument("-expn", "--experiment-name", default=None, type=str)
     parser.add_argument("-f", "--exp_file", default="exps/example/yolox_voc/yolox_voc_s.py", type=str, help="experiment description file")
-    parser.add_argument("-c", "--ckpt", default="YOLOX_outputs/yolox_voc_s/best_ckpt.pth", type=str, help="checkpoint model for evaluation")
+    parser.add_argument("-c", "--ckpt", type=str, help="checkpoint model for evaluation")
     parser.add_argument("--path", help="path to images")
     parser.add_argument("--layer", default=None, type=int, nargs='*', help="predict only specific layers")
     parser.add_argument("--conf", default=0.3, type=float, help="test confidence")
@@ -79,17 +80,56 @@ def get_image_list(path, layer=None):
                         image_list.append(image_path)
     return image_list
 
+def postprocess_output(output, img_info):
+    res = {}
+    ratio = img_info["ratio"]
+    output = output.cpu().numpy()
 
-def image_demo(predictor, vis_folder, xml_folder, path, layer, current_time, save_result, save_xml):
+    res['bboxes'] = output[:, 0:4] / ratio
+    res['classes'] = output[:, 6]
+    res['scores'] = output[:, 4] * output[:, 5] 
+    return res
+
+def image_demo(predictor, report_folder, vis_folder, xml_folder, path, layer, current_time, save_result, save_xml):
     if os.path.isdir(path):
         files = get_image_list(path, layer)
     else:
         files = [path]
     files.sort()
 
+    report_save_dir = os.path.join(report_folder, time.strftime("%Y_%m_%d_%H_%M_%S", current_time))
+    os.makedirs(report_save_dir, exist_ok=True)
+    report_path = os.path.join(report_save_dir, "InferenceReport.json")
+    result = {}
+    max_of_all_scores = 0
+
     for image_name in files:
         outputs, img_info = predictor.inference(image_name)
-        result_image = predictor.visual(outputs[0], img_info, predictor.confthre)
+
+        image_id = os.path.basename(image_name)
+        result[image_id] = {'bboxes': []}
+        result[image_id]['layer_score'] = 0
+
+        if outputs[0] is None:
+            result_image = img_info['raw_img']
+        else:
+            boxes = postprocess_output(outputs[0], img_info)
+            
+            result[image_id]['bboxes'] = process_boxes(boxes, predictor.confthre, predictor.cls_names)
+            max_score = max(boxes['scores'])
+            max_of_all_scores = max(max_of_all_scores, max_score)
+            if max_score >= predictor.confthre:
+                result[image_id]['layer_score'] = str(max_score)
+
+            result_image = vis(
+                img=img_info['raw_img'], 
+                boxes=boxes['bboxes'], 
+                scores=boxes['scores'], 
+                cls_ids=boxes['classes'],
+                conf=predictor.confthre,
+                class_names=predictor.cls_names,
+            )
+
         if save_result:
             save_folder = os.path.join(
                 vis_folder, time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
@@ -98,22 +138,35 @@ def image_demo(predictor, vis_folder, xml_folder, path, layer, current_time, sav
             save_file_name = os.path.join(save_folder, os.path.basename(image_name))
             logger.info("Saving detection result in {}".format(save_file_name))
             cv2.imwrite(save_file_name, result_image)
-        # ch = cv2.waitKey(0)
-        # if ch == 27 or ch == ord("q") or ch == ord("Q"):
-        #     break
 
         if save_xml:
             write_xml(
                 xml_folder=xml_folder, 
                 image_name=image_name, 
                 img_info=img_info, 
-                output=outputs[0], 
+                boxes = None if outputs[0] is None else postprocess_output(outputs[0], img_info),
                 cls_conf=predictor.confthre,
-                class_names=NILAR_CLASSES,
+                class_names=predictor.cls_names,
             )
+    result['defect_score_of_folder'] = str(max_of_all_scores)
+    with open(report_path, 'w') as f:
+        json.dump(result, f, indent=4, sort_keys=True)
+
+def process_boxes(boxes, cls_conf, class_names):
+    output_boxes = []
+    for i, box in enumerate(boxes['bboxes']):
+        cls_id = int(boxes['classes'][i])
+        class_name = class_names[cls_id]
+
+        score = boxes['scores'][i]
+        if score < cls_conf:
+            continue
+
+        output_boxes.append({'class': class_name, 'score': str(score), 'bbox': box.astype(int).tolist()})
+    return output_boxes
 
 
-def write_xml(xml_folder, image_name, img_info, output, cls_conf, class_names):
+def write_xml(xml_folder, image_name, img_info, boxes, cls_conf, class_names):
     root = etree.Element("annotation")
     etree.SubElement(root, "folder").text=xml_folder
     etree.SubElement(root, "filename").text=os.path.basename(image_name)
@@ -128,26 +181,19 @@ def write_xml(xml_folder, image_name, img_info, output, cls_conf, class_names):
 
     etree.SubElement(root, "segmented").text="0"
 
-    if output is not None:
-        output = output.cpu()
+    if boxes is not None:
+        for i, box in enumerate(boxes['bboxes']):
+            cls_id = int(boxes['classes'][i])
+            class_name = class_names[cls_id]
 
-        bboxes = output[:, 0:4]
-        bboxes /= img_info["ratio"] # resize
-        cls = output[:, 6]
-        scores = output[:, 4] * output[:, 5]
-
-        for i in range(len(bboxes)):
-            box = bboxes[i]
-            cls_id = int(cls[i])
-            score = scores[i]
+            score = boxes['scores'][i]
             if score < cls_conf:
                 continue
+
             x0 = int(box[0])
             y0 = int(box[1])
             x1 = int(box[2])
             y1 = int(box[3])
-
-            class_name = class_names[cls_id]
 
             object = etree.Element("object")
             etree.SubElement(object, "name").text=class_name
@@ -173,6 +219,9 @@ def main(exp, args):
         args.experiment_name = exp.exp_name
     file_name = os.path.join(exp.output_dir, args.experiment_name)
     os.makedirs(file_name, exist_ok=True)
+
+    report_folder = os.path.join(file_name, "report")
+    os.makedirs(report_folder, exist_ok=True)
 
     vis_folder = None
     if args.save_result:
@@ -222,6 +271,7 @@ def main(exp, args):
         if args.demo == "image":
             image_demo(
                 predictor=predictor, 
+                report_folder=report_folder,
                 vis_folder=vis_folder, 
                 xml_folder=xml_folder,
                 path=args.path, 
