b863a59ec25350d57c2fd070473c3cb3da1e96f6
weipeng404 <wei.peng.0404@gmail.com>
2022-05-02 08:38:00 +0200

    update number of classes.

--- exps/example/yolox_voc/yolox_voc_s.py
+++ exps/example/yolox_voc/yolox_voc_s.py
@@ -11,7 +11,7 @@ from yolox.exp import Exp as MyExp
 class Exp(MyExp):
     def __init__(self):
         super(Exp, self).__init__()
-        self.num_classes = 10
+        self.num_classes = 20
         self.input_size = (960, 1920)
         self.test_size = (960, 1920)
         self.depth = 0.33
