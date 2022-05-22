a39569e4c82887ba641670b8f580813b9e3d8362
weipeng404 <wei.peng.0404@gmail.com>
2022-05-23 01:54:38 +0200

    Add histogram equalization before training.

--- yolox/data/datasets/nilar_casing.py
+++ yolox/data/datasets/nilar_casing.py
@@ -3,6 +3,7 @@
 
 import os
 import cv2
+import numpy as np
 from .nilar import NilarDefectsDetection
 from .voc import AnnotationTransform
 
@@ -31,6 +32,23 @@ class NilarCasingDetection(NilarDefectsDetection):
 		)
 		self._imgpath = os.path.join("%s", "Images", "%s.png")
 		self._classes = CASING_CLASS
+
+	def load_resized_img(self, index):
+		img = self.load_image(index)
+
+		# histogram equalization
+		y, cr, cb = cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb))
+		y_eq = cv2.equalizeHist(y)
+		img_bgr_eq = cv2.cvtColor(cv2.merge((y_eq, cr, cb)), cv2.COLOR_YCrCb2BGR)
+    
+		r = min(self.img_size[0] / img.shape[0], self.img_size[1] / img.shape[1])
+		resized_img = cv2.resize(
+			img_bgr_eq,
+			(int(img.shape[1] * r), int(img.shape[0] * r)),
+			interpolation=cv2.INTER_LINEAR,
+		).astype(np.uint8)
+
+		return resized_img
 
 	def load_image(self, index):
 		img_id = self.ids[index]
