b07e2c7e81c1801b837f827861c37fa542b2a78d
Wei <wei.peng.0404@gmail.com>
2022-05-25 14:40:34 +0200

    Update visualize to draw output labels outside bounding box.

--- yolox/utils/visualize.py
+++ yolox/utils/visualize.py
@@ -26,18 +26,18 @@ def vis(img, boxes, scores, cls_ids, conf=0.5, class_names=None):
         txt_color = (0, 0, 0) if np.mean(_COLORS[cls_id]) > 0.5 else (255, 255, 255)
         font = cv2.FONT_HERSHEY_SIMPLEX
 
-        txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
+        txt_size = cv2.getTextSize(text, font, 1, 2)[0]
         cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
 
         txt_bk_color = (_COLORS[cls_id] * 255 * 0.7).astype(np.uint8).tolist()
         cv2.rectangle(
             img,
-            (x0, y0 + 1),
-            (x0 + txt_size[0] + 1, y0 + int(1.5*txt_size[1])),
+            (x0, y0 - int(1.5*txt_size[1]) - 1),
+            (x0 + txt_size[0] + 1, y0),
             txt_bk_color,
             -1
         )
-        cv2.putText(img, text, (x0, y0 + txt_size[1]), font, 0.4, txt_color, thickness=1)
+        cv2.putText(img, text, (x0, y0 - int(0.25*txt_size[1])), font, 1, txt_color, thickness=2)
 
     return img
 
